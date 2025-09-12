fe05911aeb0b2acb7168fd63efa6f0a6370f2e78
Pablo Ignacio Zorrilla Romo <pi.zorrillaromo@ugto.mx>
2025-09-11 19:40:32 -0600

    Actualización de archivo C de tarea sobre el método de bisección.

---
+++ Semana5/metodo_biseccion.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <math.h>
+
+float funcion1(float x) {
+    return (9.81*68.1)/x*(1-exp(-(x/68.1)*10)-40);
+}
+
+int main()
+{
+    float a, b, k, err_max, err_ir, K_old, e_puro;
+    int cont = 0;
+    print("Ingresa el valor de A: ");
+    scanf("%f", &a);
+    print("\nIngresa el valor de B: ");
+    scanf("%f", &b);
+    err_max = 0.05;
+    e_puro = 0.05;
+
+    while (((err_ir < err_max) && (e_puro < err_max)) || (cont > 200)) {
+        if (cont > 1) {
+            k = K_old;
+        }
+        k= (a+b)/2;
+        if (funcion1(k)*funcion1(a)<0) {
+            b = k;
+        } else {
+            a = k;
+        }
+        if (cont > 0) {
+            err_ir = abs((k-K_old)/k);
+
+        }
+        e_puro = (k-K_old)/(2^cont);
+    }
+
+    return 0;
+}
