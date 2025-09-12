be949ceb375361224204e628f8674dd4edaa3fd9
Pablo Ignacio Zorrilla Romo <pi.zorrillaromo@ugto.mx>
2025-09-12 11:34:11 -0600

    Arreglo de bugs en los ejercicios de la semana 6.

--- Semana6/ejercicio_clase_errores.c
+++ Semana6/ejercicio_clase_errores.c
@@ -1,10 +1,10 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <stdatomic.h>
+
 
 float funcion1(float x) {
-    return (9.81*68.1)/x*(1-exp(-(x/68.1)*10)-40);
+    return powf(x,10) - 1;
 }
 
 // Este código compara el metodo de la falsa posicion con el metodo de biseccion.
@@ -18,17 +18,19 @@ int main()
     float err_ir_falso, e_puro_falso;
     int cont = 1;
     printf("Ingresa el valor de A: ");
-    scanf("%f", &a);
-    printf("\nIngresa el valor de B: ");
-    scanf("%f", &b);
+    scanf(" %f", &a);
+    printf("Ingresa el valor de B: ");
+    scanf(" %f", &b);
     err_max = 0.05;
-    e_puro = 0.05;
+    e_puro = 0.99;
+    err_ir = 0.99;
 
-    while (((err_ir < err_max) && (e_puro < err_max)) || (cont > 200)) {
-        if (cont > 1) {
-            k = K_old;
-            k_falso = k_falso_old;
-        }
+
+
+
+
+    while (((err_ir > err_max) && (e_puro > err_max)) || (cont < 200)) {
+
         k_falso = ((funcion1(b)*a)-(funcion1(a)*b))/(funcion1(b)-funcion1(a));
         k= (a+b)/2;
         if (funcion1(k)*funcion1(a)<0) {
@@ -36,19 +38,22 @@ int main()
         } else {
             a = k;
         }
-        if (cont > 0) {
-            err_ir = abs((k-K_old)/k);
-            err_ir_falso = abs((k_falso-k_falso_old)/k_falso);
+        if (cont > 1) {
+            err_ir = fabsf((k-K_old)/k);
+            err_ir_falso = fabsf((k_falso-k_falso_old)/k_falso);
         }
-        e_puro = (k-K_old)/(2^cont);
+        e_puro = (k-K_old)/pow(2,cont);
 
+
+        k_falso_old = k_falso;
+        K_old = k;
         cont++;
     }
-
+    printf("El contador llegó a: %d", cont);
     printf("El valor obtenido para la raiz usando el metodo de biseccion es el siguiente: %f, con un error de "
            "%f", k,err_ir);
-    printf("El valor obtenido para la raiz usando el metodo de falsa posicion el siguiente: %f, con un error de"
-           "%f", k_falso, err_ir_falso);
+    printf("\n El valor obtenido para la raiz usando el metodo de falsa posicion el siguiente: %f, con un error de"
+           " %f", k_falso, err_ir_falso);
 
 
     return 0;
